94310f7ec463803065dc0dacc8f1fc3c7489134b
Ziphil <yfe23457@nifty.com>
2022-02-19 22:26:43 +0900

    年号のプレフィックスを正しく表示するよう変更

--- renderer/component/compound/clock.tsx
+++ renderer/component/compound/clock.tsx
@@ -21,26 +21,64 @@ const Clock = create(
     instant: Instant
   }): ReactElement {
 
+    let yearNode = (() => {
+      if (instant.prefix === null) {
+        let yearNode = (
+          <span className="clock-year">
+            <Letter string={instant.year ?? ""} length={4} split={true}/>
+          </span>
+        );
+        return yearNode;
+      } else {
+        let yearNode = (
+          <span className="clock-year-prefixed">
+            <Letter string={instant.prefix}/>
+            <Letter string={instant.year ?? ""} length={2} split={true}/>
+          </span>
+        );
+        return yearNode;
+      }
+    })();
     let node = (
       <div className="clock">
         <div className="clock-date">
           <div className="clock-date-full">
-            <Letter string={instant.year ?? ""} length={4} split={true}/>
-            <Letter string="/"/>
-            <Letter string={instant.month ?? ""} length={2} split={true}/>
-            <Letter string="/"/>
-            <Letter string={instant.day ?? ""} length={2} split={true}/>
+            {yearNode}
+            <span className="clock-slash">
+              <Letter string="/"/>
+            </span>
+            <span className="clock-month">
+              <Letter string={instant.month ?? ""} length={2} split={true}/>
+            </span>
+            <span className="clock-slash">
+              <Letter string="/"/>
+            </span>
+            <span className="clock-day">
+              <Letter string={instant.day ?? ""} length={2} split={true}/>
+            </span>
           </div>
           <div className="clock-date-hairia">
-            <Letter string={instant.hairia ?? ""} length={4} split={true}/>
+            <span className="clock-hairia">
+              <Letter string={instant.hairia ?? ""} length={4} split={true}/>
+            </span>
           </div>
         </div>
         <div className="clock-time">
-          <Letter string={instant.hours ?? ""} length={2} split={true}/>
-          <Letter string=":"/>
-          <Letter string={instant.minutes ?? ""} length={2} split={true}/>
-          <Letter string=":"/>
-          <Letter string={instant.seconds ?? ""} length={2} split={true}/>
+          <span className="clock-hours">
+            <Letter string={instant.hours ?? ""} length={2} split={true}/>
+          </span>
+          <span className="clock-colon">
+            <Letter string=":"/>
+          </span>
+          <span className="clock-minutes">
+            <Letter string={instant.minutes ?? ""} length={2} split={true}/>
+          </span>
+          <span className="clock-colon">
+            <Letter string=":"/>
+          </span>
+          <span className="clock-seconds">
+            <Letter string={instant.seconds ?? ""} length={2} split={true}/>
+          </span>
         </div>
       </div>
     );
