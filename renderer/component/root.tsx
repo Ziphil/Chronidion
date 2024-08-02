0dee5e0fd7add6ba15c8dc608fa5059d01321b16
Ziphil <yfe23457@nifty.com>
2024-08-02 23:38:19 +0900

    スタイルを変更

--- renderer/component/root.tsx
+++ renderer/component/root.tsx
@@ -3,13 +3,11 @@
 import * as queryParser from "query-string";
 import * as react from "react";
 import {Fragment, ReactElement, useCallback, useState} from "react";
-import {useInterval, useMount} from "react-use";
 import {useKeyEvent} from "/renderer/hook";
 import {GregorianInstant, HairianInstant, StopwatchInstant} from "/renderer/model/instant";
 import {LiteralType, LiteralUtilType} from "/renderer/util/literal-type";
 import {ClockPage} from "/renderer/component/page/clock-page";
 import {MeteoPage} from "/renderer/component/page/meteo-page";
-import {RoomPage} from "/renderer/component/page/room-page";
 import {create} from "/renderer/component/create";
 
 
@@ -20,14 +18,6 @@ const Root = create(
   }): ReactElement | null {
 
     const [mode, setMode] = useState<PageMode>("gregorian");
-
-    const updateColors = useCallback(function (): void {
-      const now = new Date();
-      const ratio = (now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds()) / 86400;
-      const hue = ratio * 360 + 270;
-      document.documentElement.style.setProperty("--first-color", getColorString(hue, 30, 80));
-      document.documentElement.style.setProperty("--second-color", getColorString(hue, 30, 50));
-    }, []);
 
     const handlePreviousPage = useCallback(function (): void {
       setMode((mode) => PageModeUtil.previous(mode));
@@ -49,31 +39,22 @@ const Root = create(
       }
     }, true);
 
-    useInterval(updateColors, 10000);
-    useMount(updateColors);
-
     const commonProps = {onPreviousPage: handlePreviousPage, onNextPage: handleNextPage};
-    const node = (
+
+    return (
       <Fragment>
         <ClockPage initialInstant={new GregorianInstant()} show={mode === "gregorian"} {...commonProps}/>
         <ClockPage initialInstant={new HairianInstant()} show={mode === "hairian"} {...commonProps}/>
         <ClockPage initialInstant={new StopwatchInstant()} show={mode === "stopwatch"} {...commonProps}/>
         <MeteoPage show={mode === "meteo"} {...commonProps}/>
-        <RoomPage show={mode === "room"} {...commonProps}/>
       </Fragment>
     );
-    return node;
 
   }
 );
 
 
-function getColorString(hue: number, saturation: number, lightness: number): string {
-  const modifiedHue = Math.floor((hue + 360) % 360);
-  return `hsl(${modifiedHue}deg, ${saturation}%, ${lightness}%)`;
-}
-
-const PAGE_MODES = ["gregorian", "hairian", "stopwatch", "meteo", "room"] as const;
+const PAGE_MODES = ["gregorian", "hairian", "stopwatch", "meteo"] as const;
 export const PageModeUtil = LiteralUtilType.create(PAGE_MODES);
 export type PageMode = LiteralType<typeof PAGE_MODES>;
 
