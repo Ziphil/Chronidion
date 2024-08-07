9221546c7bb886f2cfb78770959ec972ccb88d05
Ziphil <yfe23457@nifty.com>
2024-08-07 22:33:24 +0900

    Discordの認証をOAuth2で行うよう変更

--- main/command/discord.ts
+++ main/command/discord.ts
@@ -1,17 +1,19 @@
 //
 
 import {Client} from "discord-rpc";
+import {DiscordTokenManager} from "/main/util/discord";
 
 
+const manager = new DiscordTokenManager(process.env["DISCORD_ID"] ?? "", process.env["DISCORD_SECRET"] ?? "");
 const rpc = new Client({transport: "ipc"});
-
-const clientId = "";
-const accessToken = "";
-
-rpc.login({clientId, accessToken, scopes: ["rpc", "rpc.api", "identify"]}).catch(console.error);
 
 async function toggleMute(): Promise<void> {
   console.log("toggleMute");
+  if (!rpc.user) {
+    const clientId = manager.clientId;
+    const accessToken = await manager.getAccessToken();
+    await rpc.login({clientId, accessToken, scopes: ["rpc", "rpc.api", "identify"]});
+  }
   const settings = await rpc.getVoiceSettings();
   await rpc.setVoiceSettings({...settings, mute: !settings.mute});
 }
---
+++ main/util/discord.ts
@@ -0,0 +1,97 @@
+/* eslint-disable quote-props, @typescript-eslint/naming-convention */
+
+import {BrowserWindow} from "electron";
+import fetch from "node-fetch";
+
+
+export class DiscordTokenManager {
+
+  public readonly clientId: string;
+  public readonly clientSecret: string;
+
+  private refreshToken: string | null = null;
+
+  public constructor(id: string, secret: string) {
+    this.clientId = id;
+    this.clientSecret = secret;
+  }
+
+  public async getAuthentificationCode(): Promise<string> {
+    const promise = new Promise<string>((resolve, reject) => {
+      const window = new BrowserWindow({autoHideMenuBar: true, useContentSize: true});
+      window.webContents.on("will-navigate", (event, url) => {
+        const urlObject = new URL(url);
+        const code = urlObject.searchParams.get("code");
+        const error = urlObject.searchParams.get("error");
+        if (error) {
+          reject(new Error(`error occurred: ${error}`));
+        } else if (code) {
+          resolve(code);
+        } else {
+          reject(new Error("no code in url"));
+        }
+        window.removeAllListeners("closed");
+        setImmediate(() => window.close());
+      });
+      window.on("closed", () => {
+        reject(new Error("window closed by user"));
+      });
+      window.loadURL(`https://discord.com/oauth2/authorize?client_id=${this.clientId}&response_type=code&redirect_uri=${encodeURIComponent("http://localhost")}&scope=rpc+rpc.api+identify`);
+      window.show();
+    });
+    return promise;
+  }
+
+  public async getAccessToken(): Promise<string> {
+    if (this.refreshToken) {
+      const accessToken = this.fetchAccessTokenFromRefreshToken();
+      return accessToken;
+    } else {
+      const authentificationCode = await this.getAuthentificationCode();
+      const accessToken = this.fetchAccessTokenFromAuthentificationCode(authentificationCode);
+      return accessToken;
+    }
+  }
+
+  private async fetchAccessTokenFromAuthentificationCode(authentificationCode: string): Promise<string> {
+    const response = await fetch("https://discord.com/api/oauth2/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: new URLSearchParams({
+        "client_id": this.clientId,
+        "client_secret": this.clientSecret,
+        "grant_type": "authorization_code",
+        "code": authentificationCode,
+        "redirect_uri": "http://localhost"
+      }).toString()
+    });
+    const json = await response.json();
+    const accessToken = json["access_token"];
+    const refreshToken = json["refresh_token"];
+    this.refreshToken = refreshToken;
+    return accessToken;
+  }
+
+  private async fetchAccessTokenFromRefreshToken(): Promise<string> {
+    const response = await fetch("https://discord.com/api/oauth2/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: new URLSearchParams({
+        "client_id": this.clientId,
+        "client_secret": this.clientSecret,
+        "grant_type": "refresh_token",
+        "refresh_token": this.refreshToken ?? ""
+      }).toString()
+    });
+    const json = await response.json();
+    const accessToken = json["access_token"];
+    const refreshToken = json["refresh_token"];
+    this.refreshToken = refreshToken;
+    return accessToken;
+  }
+
+}
