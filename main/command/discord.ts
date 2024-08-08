178ef34e51948db18c3b27bdf97f1fe3f603affb
Ziphil <yfe23457@nifty.com>
2024-08-08 23:19:35 +0900

    リファクタリング

--- main/command/discord.ts
+++ main/command/discord.ts
@@ -7,7 +7,7 @@ import type {QueryName, QueryState} from "/main/command/type";
 
 
 const manager = new DiscordTokenManager(process.env["DISCORD_ID"] ?? "", process.env["DISCORD_SECRET"] ?? "");
-const rpc = new Client({transport: "ipc"});
+const client = new Client({transport: "ipc"});
 
 
 export class DiscordCommandController {
@@ -29,42 +29,42 @@ export class DiscordCommandController {
   public async toggleMute(arg: {}): Promise<void> {
     console.log("toggleMute");
     await this.ensureLogin();
-    const settings = await rpc.getVoiceSettings();
+    const settings = await client.getVoiceSettings();
     const mute = !settings.mute;
-    await rpc.setVoiceSettings({...settings, mute});
+    await client.setVoiceSettings({...settings, mute});
   }
 
   public async toggleDeaf(arg: {}): Promise<void> {
     console.log("toggleDeaf");
     await this.ensureLogin();
-    const settings = await rpc.getVoiceSettings();
+    const settings = await client.getVoiceSettings();
     const deaf = !settings.deaf;
-    await rpc.setVoiceSettings({...settings, deaf});
+    await client.setVoiceSettings({...settings, deaf});
   }
 
   public async getMute(): Promise<boolean> {
     await this.ensureLogin();
-    const settings = await rpc.getVoiceSettings();
+    const settings = await client.getVoiceSettings();
     return settings.mute;
   }
 
   public async getDeaf(): Promise<boolean> {
     await this.ensureLogin();
-    const settings = await rpc.getVoiceSettings();
+    const settings = await client.getVoiceSettings();
     return settings.deaf;
   }
 
   public async ensureLogin(): Promise<void> {
-    if (!rpc.user) {
+    if (!client.user) {
       const clientId = manager.clientId;
       const accessToken = await manager.getAccessToken();
-      await rpc.login({clientId, accessToken, scopes: ["rpc", "rpc.api", "identify"]});
-      await rpc.subscribe("VOICE_SETTINGS_UPDATE", {});
+      await client.login({clientId, accessToken, scopes: ["rpc", "rpc.api", "identify"]});
+      await client.subscribe("VOICE_SETTINGS_UPDATE", {});
     }
   }
 
   private setupEventHandlers(): void {
-    rpc.on("VOICE_SETTINGS_UPDATE", (event) => {
+    client.on("VOICE_SETTINGS_UPDATE", (event) => {
       console.log("VOICE_SETTINGS_UPDATE");
       const mute = event["mute"];
       const deaf = event["deaf"];
