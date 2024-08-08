148c1aa60342edecb87d1dac2ea4e8da97629ec7
Ziphil <yfe23457@nifty.com>
2024-08-09 00:08:31 +0900

    不要なファイルを削除

--- renderer/util/command.ts
+++
@@ -1,8 +0,0 @@
-//
-
-import type {CommandArg, CommandName} from "/main/command/type";
-
-
-export async function execCommand<N extends CommandName>(name: N, arg: CommandArg<N>): Promise<void> {
-  await window.api.invoke(`command:${name}`, arg);
-}
